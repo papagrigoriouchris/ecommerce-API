d949a8781db285b935ba7f0f4503f7947dd63362
Christoforos Papagrigoriou <home@Christofoross-MacBook-Pro.local>
2025-12-18 02:37:34 -0800

    up to 80% cover

--- __tests___/setup.js
+++ __tests___/setup.js
@@ -1,6 +1,10 @@
 const prisma = require('../src/lib/prisma');
 
+// Silence console output during tests
 beforeAll(async () => {
+  jest.spyOn(console, 'log').mockImplementation(() => {});
+  jest.spyOn(console, 'error').mockImplementation(() => {});
+
   // Clean database before all tests
   await prisma.orderItem.deleteMany();
   await prisma.order.deleteMany();
@@ -9,6 +13,10 @@ beforeAll(async () => {
 });
 
 afterAll(async () => {
+  // Restore console
+  console.log.mockRestore();
+  console.error.mockRestore();
+
   // Disconnect prisma after all tests
   await prisma.$disconnect();
 });
